05113413d173efd2bae966fbbe9e58a10aa9d87d
VuNguyenPhuongTuyet <vunguyenphuong2010@gmail.com>
2021-09-15 22:13:43 +0700

    CORS

--- charity/src/main/java/com/tuyet/charity/CharityApplication.java
+++ charity/src/main/java/com/tuyet/charity/CharityApplication.java
@@ -12,14 +12,4 @@ public class CharityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CharityApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
 }
